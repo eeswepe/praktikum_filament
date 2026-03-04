cb8ed1a87d438555d8e329ca6c129a1fd6da6586
eeswepe <singgihwahyupermana@gmail.com>
2026-03-05 01:34:37 +0700

    feat: finish 3rd jobsheet

---
+++ app/Filament/Resources/Categories/Schemas/CategoryForm.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Filament\Resources\Categories\Schemas;
+
+use Filament\Schemas\Schema;
+use Filament\Forms\Components\TextInput;
+
+class CategoryForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema->components([
+            TextInput::make("nama")->label("nama"),
+            TextInput::make("slug")->label("slug")->unique(),
+        ]);
+    }
+}
---
+++ app/Filament/Resources/Categories/Tables/CategoriesTable.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Filament\Resources\Categories\Tables;
+
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\EditAction;
+use Filament\Tables\Table;
+use Filament\Tables\Columns\TextColumn;
+
+class CategoriesTable
+{
+    public static function configure(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make("nama")->label("Nama"),
+                TextColumn::make("slug")->label("Slug"),
+            ])
+            ->filters([
+                //
+            ])
+            ->recordActions([EditAction::make()])
+            ->toolbarActions([
+                BulkActionGroup::make([DeleteBulkAction::make()]),
+            ]);
+    }
+}
